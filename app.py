ad31c05d1dd67f9414e61d264e59c68d91d26df7
KN-20 <2020112323@student.kyungnam.ac.kr>
2023-10-06 10:14:56 +0900

    ai test

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/ImgSend', methods = ['POST', 'GET'])
+def ImgSend():
+    if request.method == 'POST':
+        id = request.form['id']
+    return 0
+
+@app.route('/')
+def test():
+    print('test')
+    return '<h1>Hello World<h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5501)
