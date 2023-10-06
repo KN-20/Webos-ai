d2e614094d209b45521f1ba4b889d922e8c7f4a2
KN-20 <2020112323@student.kyungnam.ac.kr>
2023-10-06 13:13:39 +0900

    ai 테스트 추가

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 @app.route('/ImgSend', methods = ['POST', 'GET'])
 def ImgSend():
     if request.method == 'POST':
-        id = request.form['id']
-        print(id)
+        test_file = request.form['test']
+        print(test_file)
         return "POST TEST DATA: %s" %id
     elif request.method == 'GET':
-        test_json = request.args.get('test')
-        print(type(test_json))
-        print(test_json)
-        return "GET TEST DATA: %s" %test_json
+        test_file = request.args.get('test')
+        print(type(test_file))
+        print(test_file)
+        return "GET TEST DATA: %s" %test_file
 
 @app.route('/')
 def test():
